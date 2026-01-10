4cf966321981fdba7d3d71e8660f711a9c580eb2
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2026-01-10 15:42:13 +0530

    chore: update type definitions

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,21 @@
+import { StaticImageData } from "next/image";
+
 export interface ExperienceItem {
 	name: string;
 	time: string;
+	logo?: string | StaticImageData;
 	isPresent: boolean;
 	designation: string;
 	description: string[];
+}
+
+export interface ProjectItem {
+	name: string;
+	description: string;
+	tech: string[];
+	github: string;
+	url: string;
+	time: string;
+	bgImage: StaticImageData;
+	image: StaticImageData;
 }
